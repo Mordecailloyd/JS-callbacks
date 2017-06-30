ba080fbfd9fae74ad4457e8473aff18789498671
Nicholas Bacon <Mordecailloyd@gmail.com>
2017-06-29 17:54:24 -0700

    cleaned Towers of Hanoi

--- hanoi.js
+++ hanoi.js
@@ -4,12 +4,14 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
+
+
 class Game {
   constructor() {
     this.stacks = [[3,2,1],[],[]];
-    // this.run();
   }
 
+  // allows invalid move 
   isValidMove(startIdx, endIdx) {
     let lastVal = startIdx.length-1;
     let lastVal2 = endIdx.length-1;
@@ -26,8 +28,6 @@ class Game {
   }
 
   run(){
-    // console.log(this);
-    // console.log(this.stacks);
     this.promptMove((startStack,endStack)=> {
       this.move(startStack, endStack);
       // console.log(this);
@@ -38,53 +38,30 @@ class Game {
         console.log('Game over!');
       }
     });
-    // console.log ( this.isWon)
-    // reader.question("Would you like to play again?", function(answer) {
-    //   if (answer === 'yes'){
-    //     let newgame = new Game();
-    //   }
-    // });
-    // console.log('Game over!');
   }
 
   move(startStack, endStack){
-    // console.log(this);
-    // console.log(startStack);
-    // console.log(endStack);
-    // console.log(this.isValidMove(startStack,endStack));
     if (this.isValidMove(startStack, endStack)) {
-      console.log (this.stacks);
       let pop = this.stacks[startStack].pop();
-      console.log(pop);
       this.stacks[endStack].push(pop);
-      // this.promptMove(this.move.bind(this));
-      // }
     }
   }
 
   promptMove(callback) {
     console.log(JSON.stringify(this.stacks));
-    reader.question("Which stack would you like to move from?", function(startStack) {
-      reader.question("Which stack would you like to move to?", function(endStack) {
+    reader.question("Which stack would you like to move from?",
+     function(startStack) {
+      reader.question("Which stack would you like to move to?",
+       function(endStack) {
         startStack = parseInt(startStack);
         endStack = parseInt(endStack);
-        // console.log(this);
         callback(startStack,endStack);
-        // this.move(startStack, endStack);
-        // if (this.isValidMove(startStack, endStack)) {
-        //   this.stacks[endStack].push(this.stacks[startStack].pop);
-        //   if (this.isWon()) {
-        //     break
-        //   }
-        // }
       });
     });
-
-    // this.promptMove();
   }
 
   isWon() {
-    if (this.stacks[2][-1] === 1) {
+    if (this.stacks[2][2] === 1) {
       if (this.stacks[2][0] === 3){
         console.log("You Win!");
         reader.close;
@@ -95,4 +72,3 @@ class Game {
   }
 }
 module.exports = Game;
-// var gar = new Game();
